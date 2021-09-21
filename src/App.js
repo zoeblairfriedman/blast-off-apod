3faf285592b8621c8f87d9779000a11a9c6799c4
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-21 17:39:56 -0400

    testing change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Blast off
+          TESTING CHANGE! 
         </p>
         <a
           className="App-link"
